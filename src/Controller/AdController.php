6c59a13c69f774ee2e991af82146063e38f6554c
Matthieu REUNGOAT <reungoat.matthieu@gmail.com>
2023-05-26 16:47:59 +0200

    working with twig

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,30 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(): Response
+    public function index(AdRepository $repo): Response
     {
-        return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
-        ]);
+        $ads = $repo->findAll();
+
+        return $this->render(
+            'ad/index.html.twig',
+            [
+                'ads' => $ads,
+            ]);
+    }
+
+    /**
+     * @Route("/ads/{slug}",name="ads_show")
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo)
+    {
+        $ad = $repo->findOneBySlug($slug);
+
+        return $this->render(
+            'ad/show.html.twig',
+            [
+                'ad' => $ad
+            ]
+        );
     }
 }
